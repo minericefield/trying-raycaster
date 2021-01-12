99ce041fd140512d7d4c3ea04c3c132013a6d27e
minericefield <minericefield@gmail.com>
2021-01-13 00:03:34 +0900

    initialising camera

--- src/modules/three.js
+++ src/modules/three.js
@@ -1,17 +1,21 @@
 import { Scene } from 'three'
 import { reactive, toRefs } from 'vue'
 
+import { initializeCamera } from '@/modules/three/camera'
 import { initializeScale } from '@/modules/three/scale'
 
 export const initializeThree = (baseSize) => {
   const { fullZDistance, fullFarWidth, fullFarHeight } = initializeScale(baseSize)
 
   const three = reactive({
-    scene: null
+    scene: null,
+    camera: null
   })
 
   const execute = () => {
     three.scene = new Scene()
+
+    three.camera = initializeCamera(baseSize, fullZDistance)
   }
 
   return {
