14dc9108c3310e29c489cd1ae1a533686f21d401
minericefield <minericefield@gmail.com>
2021-01-14 01:48:45 +0900

    fix rotationSpeed to use baseSize

--- src/modules/three.js
+++ src/modules/three.js
@@ -43,7 +43,7 @@ export const initializeThree = (baseSize, additionalAnimationFrameMethod) => {
     textsToReactive()
     scene.add(texts.textGroup)
 
-    rotationSpeed = initializeRotationSpeed(fullZDistance)
+    rotationSpeed = initializeRotationSpeed(baseSize)
 
     render()
   }
--- src/modules/three/rotationSpeed.js
+++ src/modules/three/rotationSpeed.js
@@ -1,8 +1,8 @@
 import { Vector3 } from 'three'
 
-export const initializeRotationSpeed = (fullZDistance) => {
+export const initializeRotationSpeed = (baseSize) => {
   const vector3 = new Vector3()
-  const isOnFrontCenter = ({ x, y, z }) => x > -fullZDistance.value / 20 && x < 1 && y > -fullZDistance.value / 20 && y < 1 && z > 1
+  const isOnFrontCenter = ({ x, y, z }) => x > -baseSize.width.value / 22 && x < 1 && y > -baseSize.height.value / 22 && y < 1 && z > 1
 
   const getSpeed = (targets) => {
     if (targets.find(target => isOnFrontCenter(target.getWorldPosition(vector3)))) {
